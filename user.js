6208b44d8d299c3124b5902bf06a97e3d28962b1
jacabaca22 <jacekrojek11@gmail.com>
2026-04-04 18:16:37 +0200

    Logika logowanie i rejestracja

--- user.js
+++ user.js
@@ -1,3 +1,13 @@
 function rozsunKarte(element) {
   element.classList.toggle('active');
 }
+const enterLogowanie = document.getElemendById('enter-Logowanie');
+const Login = document.getElementById('Login');
+const Password = document.getElementById('Password');
+const Email = document.getElementById('Email');
+function validateForm(){
+  if (enterLogowanie.value === '') {
+    alert('Please fill in all fields');
+    return false;
+  }
+}
